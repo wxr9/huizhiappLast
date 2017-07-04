626f390028a76917ce49ba248c0fec280f6f967e
ha1bo <haizhongdebo@163.com>
2017-07-04 17:56:50 +0800

    提交

--- huizhiApp/.roadhogrc.mock.js
+++
@@ -1,7 +0,0 @@
-
-export default {
-  "/api/user":{
-    "username": 'wise',
-    "age": 30
-  }
-};
--- huizhiApp/src/services/example.js
+++
@@ -1,5 +0,0 @@
-import request from '../utils/request';
-
-export async function query() {
-  return request('http://222.73.203.71:8080/mockjs/1/j_spring_security_logout');
-}
